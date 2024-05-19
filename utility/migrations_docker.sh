b2a1618a83849187a77311603ad1732c7ce033ba
Pedro Ulisses <pedro.ulisses@conceptu.ind.br>
2024-05-19 15:24:59 -0300

    ok

--- utility/migrations_docker.sh
+++ utility/migrations_docker.sh
@@ -2,6 +2,5 @@
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 COMPOSE_DIR="$SCRIPT_DIR/../compose"
 cd "$COMPOSE_DIR" || exit
-python manage.py makemigrations
 docker-compose -f local.yml exec web python manage.py makemigrations
 docker-compose -f local.yml exec web python manage.py migrate
