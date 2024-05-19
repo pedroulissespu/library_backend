657fb54fcf868c587f6d57f85218a2d3b0157989
Pedro Ulisses <pedro.ulisses@conceptu.ind.br>
2024-05-18 21:00:10 -0300

    fix

--- library/authors/tests/tests.py
+++ library/authors/tests/tests.py
@@ -1,3 +1,40 @@
 from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
+from ..models import Author
 
-# Create your tests here.
+
+class AuthorTests(TestCase):
+    fixtures = ['authors/fixtures/authors.json']
+
+    def test_get_authors_list(self):
+        url = reverse('author-list')
+        response = self.client.get(url, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data), 2)
+
+    def test_get_author_detail(self):
+        url = reverse('author-detail', args=[1])
+        response = self.client.get(url, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['name'], 'Author One')
+
+    def test_create_author(self):
+        url = reverse('author-list')
+        data = {'name': 'Author Three', 'birthdate': '1990-01-01'}
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(Author.objects.count(), 3)
+
+    def test_update_author(self):
+        url = reverse('author-detail', args=[1])
+        data = {'name': 'Author One Updated', 'birthdate': '1970-01-01'}
+        response = self.client.put(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(Author.objects.get(pk=1).name, 'Author One Updated')
+
+    def test_delete_author(self):
+        url = reverse('author-detail', args=[1])
+        response = self.client.delete(url)
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+        self.assertEqual(Author.objects.count(), 1)
--- library/books/tests/tests.py
+++ library/books/tests/tests.py
@@ -1,33 +1,40 @@
 from django.test import TestCase
 from django.urls import reverse
 from rest_framework import status
-
-from library.authors.models import Author
 from ..models import Book
 
 
 class BookTests(TestCase):
+    fixtures = ['books/fixtures/books.json', 'authors/fixtures/authors.json']
 
-    def setUp(self):
-        self.author = Author.objects.create(first_name="John", last_name="Doe")
-        self.book = Book.objects.create(
-            title="Test Book",
-            author=self.author,
-            published_date="2023-01-01",
-            isbn="1234567890123",
-            page_count=100,
-            language="EN"
-        )
-        self.url = reverse('book-list')
+    def test_get_books_list(self):
+        url = reverse('book-list')
+        response = self.client.get(url, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data), 2)
+
+    def test_get_book_detail(self):
+        url = reverse('book-detail', args=[1])
+        response = self.client.get(url, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['title'], 'Book One')
 
     def test_create_book(self):
-        data = {
-            "title": "New Book",
-            "author": self.author.id,
-            "published_date": "2023-01-01",
-            "isbn": "1234567890123",
-            "page_count": 200,
-            "language": "EN"
-        }
-        response = self.client.post(self.url, data, format='json')
+        url = reverse('book-list')
+        data = {'title': 'Book Three', 'publication_date': '2020-01-01', 'author': 1}
+        response = self.client.post(url, data, format='json')
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(Book.objects.count(), 3)
+
+    def test_update_book(self):
+        url = reverse('book-detail', args=[1])
+        data = {'title': 'Book One Updated', 'publication_date': '2000-01-01', 'author': 1}
+        response = self.client.put(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(Book.objects.get(pk=1).title, 'Book One Updated')
+
+    def test_delete_book(self):
+        url = reverse('book-detail', args=[1])
+        response = self.client.delete(url)
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+        self.assertEqual(Book.objects.count(), 1)
