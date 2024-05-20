7d20368c9a998ae9008d1b05a0aac8135af13d34
Pedro Ulisses <pedro.ulisses@conceptu.ind.br>
2024-05-20 11:11:15 -0300

    update pre-commit

--- config/urls.py
+++ config/urls.py
@@ -15,12 +15,17 @@ urlpatterns = [
     path(f"{API_PREFIX}", include("config.api_router"), name="api-root"),
     path(f"admin/", admin.site.urls),
     path(f"{API_PREFIX}users/", UserCreate.as_view(), name="user_create"),
-    path(f"{API_PREFIX}books/", include('library.books.api.urls')),
-    path(f"{API_PREFIX}authors/", include('library.authors.api.urls')),
-    path(f"{API_PREFIX}auth/", include('rest_framework.urls')),
-    path(f"{API_PREFIX}token/", TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path(f"{API_PREFIX}token/refresh/", TokenRefreshView.as_view(), name='token_refresh'),
-    path(f"{API_PREFIX}token/verify/", TokenVerifyView.as_view(), name='token_verify'),
+    path(f"{API_PREFIX}books/", include("library.books.api.urls")),
+    path(f"{API_PREFIX}authors/", include("library.authors.api.urls")),
+    path(f"{API_PREFIX}borrow/", include("library.borrow.api.urls")),
+    path(f"{API_PREFIX}auth/", include("rest_framework.urls")),
+    path(
+        f"{API_PREFIX}token/", TokenObtainPairView.as_view(), name="token_obtain_pair"
+    ),
+    path(
+        f"{API_PREFIX}token/refresh/", TokenRefreshView.as_view(), name="token_refresh"
+    ),
+    path(f"{API_PREFIX}token/verify/", TokenVerifyView.as_view(), name="token_verify"),
     path(f"{API_PREFIX}schema/", SpectacularAPIView.as_view(), name="api-schema"),
     path(
         f"{API_PREFIX}docs/",
